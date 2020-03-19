8ca545b7abea061e54c14a4988d43800d7259728
dev-sm <saimain.dev@gmail.com>
2020-03-19 16:07:46 +0630

    s

--- app/Http/Controllers/CountryData.php
+++ app/Http/Controllers/CountryData.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\CountryListResource;
 use App\Http\Resources\CountryResource;
 use App\Http\Resources\NotFoundResource;
 use Illuminate\Http\Request;
@@ -11,18 +12,22 @@ class CountryData extends Controller
     public function getData($iso)
     {
         $client = new \GuzzleHttp\Client();
-        try {
-            $response = $client->request('GET', 'https://covid19.mathdro.id/api/countries/' . $iso);
-            $array = json_decode($response->getBody()->getContents(), true); //
-            return new CountryResource($array);
-        } catch (\Throwable $th) {
-            // return $th;
-            // 
-            $error =  [
-                ['text' => 'Data not found'],
-            ];
-            return new NotFoundResource($error);
-        }
+        $countries = $client->request('GET', 'https://covid19.mathdro.id/api/countries/');
+        $countries_array = json_decode($countries->getBody()->getContents(), true);
+        return new CountryListResource($countries_array);
+
+        // try {
+        //     $response = $client->request('GET', 'https://covid19.mathdro.id/api/countries/' . $iso);
+        //     $array = json_decode($response->getBody()->getContents(), true); //
+        //     return new CountryResource($array);
+        // } catch (\Throwable $th) {
+        //     // return $th;
+        //     // 
+        //     $error =  [
+        //         ['text' => $iso . 'Data not found'],
+        //     ];
+        //     return new NotFoundResource($error);
+        // }
 
         // $array = json_decode($response->getBody()->getContents(), true); //
 
